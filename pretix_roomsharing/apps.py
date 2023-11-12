85d6b754e0091e0fffb3e99548249741fdb30d54
techwolf12 <contact@techwolf12.nl>
2023-11-12 14:01:18 +0100

    Remove depricated Django

--- pretix_roomsharing/apps.py
+++ pretix_roomsharing/apps.py
@@ -1,7 +1,11 @@
-from django.apps import AppConfig
 from django.utils.translation import gettext_lazy
+
 from . import __version__
 
+try:
+    from pretix.base.plugins import PluginConfig
+except ImportError:
+    raise RuntimeError("Please use pretix 2.7 or above to run this plugin!")
 
 class PluginApp(PluginConfig):
     name = "pretix_roomsharing"
--- pretix_roomsharing/urls.py
+++ pretix_roomsharing/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 
 from .views import (
     ControlRoomChange,
@@ -12,45 +12,45 @@ from .views import (
 )
 
 urlpatterns = [
-    url(
+    re_path(
         r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/roomsharing/",
         SettingsView.as_view(),
         name="control.room.settings",
     ),
-    url(
+    re_path(
         r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/orders/(?P<code>[0-9A-Z]+)/room$",
         ControlRoomChange.as_view(),
         name="control.order.room.modify",
     ),
     url(
-        r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/rooms/stats/$",
+        r"control/event/<str:organizer>/<str:event>/rooms/stats/",
         StatsView.as_view(),
         name="event.stats",
     ),
     url(
-        r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/rooms/$",
+        r"control/event/<str:organizer>/<str:event>/rooms/",
         RoomList.as_view(),
         name="event.room.list",
     ),
     url(
-        r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/rooms/(?P<pk>\d+)/$",
+        r"control/event/<str:organizer>/<str:event>/rooms/<int:pk>/",
         RoomDetail.as_view(),
         name="event.room.detail",
     ),
     url(
-        r"^control/event/(?P<organizer>[^/]+)/(?P<event>[^/]+)/rooms/(?P<pk>\d+)/delete$",
+        r"control/event/<str:organizer>/<str:event>/rooms/<int:pk>/delete",
         RoomDelete.as_view(),
         name="event.room.delete",
     ),
     url(
-        r"^metrics/rooms/(?P<organizer>[^/]+)/(?P<event>[^/]+)/$",
+        r"metrics/rooms/<str:organizer>/<str:event>/",
         MetricsView.as_view(),
         name="metrics",
     ),
 ]
 
 event_patterns = [
-    url(
+    re_path(
         r"^order/(?P<order>[^/]+)/(?P<secret>[A-Za-z0-9]+)/room/modify$",
         OrderRoomChange.as_view(),
         name="event.order.room.modify",
